91599ec8c86c46c663b5309d1c4ded176ea6d7a9
Sanjuro Jogdeo <weisenheimer@sanjuro.net>
2021-11-05 07:56:08 -0700

    Finish library demo.

--- library/views.py
+++ library/views.py
@@ -9,20 +9,25 @@ def search_by_isbn(request):
 
     if request.method == 'POST':
         results = []
-        isbn_numbers = request.POST.get('isbn_numbers').split('\n')
-        api_base = 'https://openlibrary.org/isbn/{}.json'
-        for isbn_number in isbn_numbers:
-            if isbn_number.strip() == '':
-                continue
-            api_url = api_base.format(isbn_number)
-            response = requests.get(api_url)
-            if response.status_code != 200:
-                results.append([isbn_number, 'Not found'])
-            else:
-                content = json.loads(response.content)
+        isbn_numbers = [isbn.strip() for isbn in request.POST.get('isbn_numbers').split('\n') if isbn != '']
+
+        api_base = 'https://openlibrary.org/api/books?format=json&jscmd=data&bibkeys='
+        bibkeys = ','.join([f'ISBN:{i}' for i in isbn_numbers])
+
+        api_url = api_base + bibkeys
+        response = requests.get(api_url)
+        if response.status_code != 200:
+            results.append("There was an error. Flog the developer.")
+        else:
+            content = json.loads(response.content)
+            for isbn_number in isbn_numbers:
+                content_key = 'ISBN:' + str(isbn_number)
                 try:
-                    results.append([isbn_number, content['title']])
+                    book_content = content[content_key]
+                    subjects = ', '.join([s['name'] for s in book_content['subjects']])
+                    results.append((isbn_number, book_content['title'], subjects))
+
                 except KeyError:
-                    results.append([isbn_number, 'Error'])
+                    results.append((isbn_number, 'Error', None))
 
         return render(request, 'sanjuroville/search_by_isbn_results.html', context={'results': results})
